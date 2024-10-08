39cd76a9047648cfccda3887585997a83997172f
zebin huang <1667535930@qq.com>
2024-10-09 01:44:49 +0800

    启动

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// import LoginPage from '../components/LoginPage.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: LoginPage
+    },
+
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
