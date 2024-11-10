176ec18c37d60e3a8ab2b925e1f1dc84c5163cea
logic-xx <1561336316@qq.com>
2024-11-10 20:46:41 +0800

    忘记密码过程以及美化

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import router from './router';  // 引入 router
 //引入elementPlus
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 
 
 createApp(App).use(ElementPlus).use(router).mount('#app');
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    App.component(key, component)
+}
 
 
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,31 +26,124 @@
             </el-main>
 
             <!-- 登录浮窗 -->
-            <el-dialog v-model="loginDialogVisible" title="登录" width="500" align-center>
-                <el-form>
-                    <el-radio-group  v-model="selectedCity" style="margin-bottom: 30px">
-                    <el-radio-button value="1">top</el-radio-button>
-                    <el-radio-button value="right">right</el-radio-button>
-                    <el-radio-button value="bottom">bottom</el-radio-button>
-                    <el-radio-button value="left">left</el-radio-button>
-                </el-radio-group>
+<!--            <el-dialog v-model="loginDialogVisible" title="登录" width="500" center>-->
+<!--                <el-form>-->
+<!--                    <el-radio-group  v-model="selectedCity" style="margin-bottom: 30px">-->
+<!--                    <el-radio-button value="1">top</el-radio-button>-->
+<!--                    <el-radio-button value="right">right</el-radio-button>-->
+<!--                    <el-radio-button value="bottom">bottom</el-radio-button>-->
+<!--                    <el-radio-button value="left">left</el-radio-button>-->
+<!--                </el-radio-group>-->
 
-                    <el-form-item>
-                        <el-input placeholder="登录邮箱"></el-input>
+<!--                    <el-form-item label="账号">-->
+<!--                        <el-input placeholder="输入邮箱/用户名"></el-input>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item label="密码">-->
+<!--                        <el-input type="password" placeholder="输入密码"></el-input>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item>-->
+<!--                        <el-checkbox>记住密码</el-checkbox>-->
+<!--                        <a href="#">忘记密码</a>-->
+<!--                    </el-form-item>-->
+<!--                </el-form>-->
+<!--                <div slot="footer" class="dialog-footer">-->
+<!--                    <el-button @click="loginDialogVisible = false">取 消</el-button>-->
+<!--                    <el-button type="primary" >登 录</el-button>-->
+<!--                </div>-->
+
+<!--            </el-dialog>-->
+            <el-dialog v-model="loginDialogVisible"
+                       title="登录"
+                       width="500px"
+                       align-center>
+
+                <el-form label-position="top" class="login-form">
+                    <el-radio-group v-model="selectedCity" style="margin-bottom: 30px">
+                        <el-radio-button value="1">学生</el-radio-button>
+                        <el-radio-button value="right">教师</el-radio-button>
+                        <el-radio-button value="bottom">学校管理员</el-radio-button>
+                        <el-radio-button value="left">系统管理员</el-radio-button>
+                    </el-radio-group>
+
+                    <el-form-item  class="form-item">
+                        <el-input
+                            v-model="input1"
+                            style="width: 280px"
+                            placeholder="输入邮箱/用户名"
+                            :prefix-icon="User">
+                        </el-input>
                     </el-form-item>
-                    <el-form-item>
-                        <el-input type="password" placeholder="登录密码"></el-input>
+                    <el-form-item  class="form-item">
+                        <el-input
+                            v-model="input2"
+                            style="width: 280px"
+                            type="password"
+                            placeholder="输入密码"
+                            show-password
+                            >
+                        </el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item class="form-item">
                         <el-checkbox>记住密码</el-checkbox>
-                        <a href="#">忘记密码</a>
+                        <a href="#" style="float: right;" @click="forgotPasswordDialogVisible = true">忘记密码</a>
                     </el-form-item>
                 </el-form>
                 <div slot="footer" class="dialog-footer">
-                    <el-button @click="loginDialogVisible = false">取 消</el-button>
-                    <el-button type="primary" >登 录</el-button>
+                    <el-button type="primary" class="custom-button">登 录</el-button>
                 </div>
+            </el-dialog>
 
+<!--            找回密码-->
+            <el-dialog v-model="forgotPasswordDialogVisible"
+                       title="找回密码"
+                       width="500px"
+                       align-center
+                       >
+
+                <el-steps style="max-width: 600px; margin-bottom: 60px" :active="active"
+                          finish-status="success" simple>
+                    <el-step title="Step 1" />
+                    <el-step title="Step 2" />
+                    <el-step title="Step 3" />
+                </el-steps>
+
+
+                <el-form label-position="right" label-width="120px" class="form-item">
+                    <!-- Step1 -->
+                    <div v-if="active === 1">
+                        <el-form-item label="邮箱" class="form-item">
+                            <el-input v-model="mail" placeholder="请输入您的邮箱" style="width: 250px"></el-input>
+                        </el-form-item>
+
+                        <el-form-item label="验证码" class="form-item">
+                            <el-input v-model="verifyCode" placeholder="请输入验证码" style="width: 150px"></el-input>
+                            <el-button >发送验证码</el-button>
+                        </el-form-item>
+                    </div>
+                    <!-- Step2 -->
+                    <div v-if="active === 2">
+                        <el-form-item label="新密码" class="form-item">
+                            <el-input v-model="mail" placeholder="请输入新密码" style="width: 250px"></el-input>
+                        </el-form-item>
+
+                        <el-form-item label="确认新密码" class="form-item">
+                            <el-input v-model="verifyCode" placeholder="请重复输入密码" style="width: 250px"></el-input>
+                        </el-form-item>
+                    </div>
+                    <!-- Step3 -->
+                    <div v-if="active === 3" class="center-text">
+                        <p>成功重置密码</p>
+                    </div>
+                    <!-- 其他表单项 -->
+                </el-form>
+
+                <div slot="footer" class="dialog-footer1">
+                    <el-button v-if="active > 1 && active < 3" @click="front">frontStep</el-button>
+                    <el-button v-if="active < 3" @click="next">nextStep</el-button>
+                    <el-button v-if="active == 3" @click="goToOwnPage" type="primary" class="custom-button">进入平台</el-button>
+
+
+                </div>
             </el-dialog>
 
 
@@ -62,10 +155,28 @@
 <script lang="ts" setup>
 
 import { ref } from 'vue'
+import {User,Lock} from "@element-plus/icons-vue";
 
 const loginDialogVisible = ref(false)
+const forgotPasswordDialogVisible = ref(false)
 const userType = ref<'student' | 'teacher' | 'admin' | 'sysadmin'>('student');
 const selectedCity = ref('1') // 默认选中的城市
+const input1 = ref('')
+const input2 = ref('')
+const mail = ref('')
+const verifyCode = ref('')
+const active = ref(1)
+
+const next = () => {
+    if (active.value++ > 2) active.value = 0
+}
+const front = () => {
+    if (active.value-- < 1) active.value = 0
+}
+const goToOwnPage = () => {
+
+}
+
 
 </script>
 
@@ -115,5 +226,66 @@ const selectedCity = ref('1') // 默认选中的城市
     height: 100%; /* 使图片高度填满carousel-item */
     object-fit: fill; /* 确保图片按比例填充 */
 }
+
+.login-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center; /* 垂直居中 */
+    align-items: center; /* 水平居中 */
+    height: 100%; /* 使表单占满整个对话框的高度 */
+}
+
+
+
+.dialog-footer .el-button {
+    margin-left: 10px;
+}
+
+.form-item {
+    margin-bottom: 30px;
+}
+
+.dialog-footer {
+    display: flex;
+    justify-content: center;
+}
+.dialog-footer1 {
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+}
+
+.dialog-footer .el-button {
+    margin-left: 10px;
+}
+
+/* 使忘记密码链接对齐到右侧 */
+.el-form-item__content a {
+    margin-left: 80px; /* 为忘记密码链接添加左边距 */
+}
+
+.custom-button {
+    height: 40px; /* 设置按钮的高度 */
+    line-height: 40px; /* 设置按钮文本的垂直居中 */
+    width: 300px; /* 设置按钮的宽度 */
+    padding: 0; /* 去除默认的内边距 */
+}
+
+
+.centered-form .el-form-item {
+    display: flex;
+    align-items: center; /* 垂直居中 */
+    justify-content: center; /* 水平居中 */
+    width: 90%; /* 使表单项宽度填满父容器 */
+    margin-top: 20px; /* 增加表单项之间的间距 */
+    margin-bottom: 0px; /* 增加表单项之间的间距 */
+}
+.center-text {
+    margin-top: 100px;
+    margin-bottom: 100px;
+    text-align: center;
+    font-size: 24px; /* 你可以根据需要调整字体大小 */
+}
+
 
 </style>
