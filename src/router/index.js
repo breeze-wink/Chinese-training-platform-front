cd75149c540997c9f9b604bc0c77af16cff22605
Sherlock <835975242@qq.com>
2024-11-14 22:04:43 +0800

    Merge branch 'main' into Sherlock

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ import TeacherPersonalInfo from '../pages/Teacher/PersonalInfo.vue';// 老师个
 import ViewCurriculumStandard from "@/pages/Teacher/ViewCurriculumStandard.vue";
 
 import KnowledgePoint from '../pages/system-admin/KnowledgePoint.vue';
-
+import StudentPersonalInfo from '../pages/Student/PersonalInfo.vue';
+import PoetryList from "@/pages/Student/PoetryList.vue";
 
 const routes = [
     //首页
@@ -15,6 +16,9 @@ const routes = [
     { path: '/teacher/view-curriculum-standard', component: ViewCurriculumStandard},
     //系统管理员
     { path: '/system-admin/KnowledgePoint', component: KnowledgePoint },
+    // 学生的个人页面
+    { path: '/student/personal-info', component: StudentPersonalInfo},
+    { path: '/student/poetry-list', component: PoetryList},
 
 ];
 
