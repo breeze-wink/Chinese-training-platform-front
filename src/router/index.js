a90d70ff1866f8dff813d47858248b53ee605779
logic-xx <1561336316@qq.com>
2024-11-14 09:40:20 +0800

    老师跟人信息界面（内容完善）

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue'
 import TeacherPersonalInfo from '../pages/Teacher/PersonalInfo.vue';// 老师个人信息
-import KnowledgePoint from '../pages/system-admin/KnowledgePoint.vue';
+// import KnowledgePoint from '../pages/system-admin/KnowledgePoint.vue';
 
 const routes = [
     //首页
     { path: '/', component: Home },
     //老师的个人信息页面
     { path: '/teacher/personal-info', component: TeacherPersonalInfo },
-    { path: '/system-admin/KnowledgePoint', component: KnowledgePoint }
+    //{ path: '/system-admin/KnowledgePoint', component: KnowledgePoint }
 
 ];
 
