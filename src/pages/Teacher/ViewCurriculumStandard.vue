fc341bf8f1c4aa4e21e5d15f169d8409726ceee6
Aozora <2271213161@qq.com>
2024-12-26 01:03:18 +0800

    refactor(ViewCurriculumStandard):移除课标获取成功后的提示信息
    - 删除了课程标准获取成功后显示的 ElMessage 提示
    - 优化了用户体验，避免了不必要的弹窗干扰

--- src/pages/Teacher/ViewCurriculumStandard.vue
+++ src/pages/Teacher/ViewCurriculumStandard.vue
@@ -61,7 +61,7 @@ const getAllCourseStandards = async () => {
         const response = await axios.get('/api/teacher/get-all-course-standards');
         if (response.status === 200) {
             courseStandards.value = response.data.courseStandardInfos;
-            ElMessage({ message: '课标获取成功', type: 'success' });
+
         }
     } catch (error) {
         ElMessage({ message: '课标获取失败', type: 'error' });
